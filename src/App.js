269e5a08b734d00a9f13de40a167c82bcce2162b
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-16 19:58:41 +0100

    Passed state by props to Pitch

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Ultimate Team App</h1>
-        <Pitch />
+        <Pitch players={this.state.players} />
       </div>
     );
   }
