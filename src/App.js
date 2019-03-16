8456ee7b42a08282aa942f8a8167f92953fab29c
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-16 19:37:58 +0100

    Change css to scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
+import Square from "./Components/Square/Square";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Ultimate Team App</h1>
+        <Square />
       </div>
     );
   }
