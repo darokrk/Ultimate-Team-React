d8f370ad0078a29a4fb6bbbbc5418f372505cd5b
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-16 21:35:00 +0100

    Styled bench players

--- src/Components/Bench/Bench.js
+++ src/Components/Bench/Bench.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Player from "../Player/Player";
 import "./Bench.scss";
 
-const Bench = () => {
-  return (
-    <div>
-      <p>bench players</p>
-    </div>
-  );
+const Bench = props => {
+  const playersOnBench = props.benchPlayers.map((player, i) => (
+    <Player key={player.name} player={player} index={i} />
+  ));
+  return <div className="bench">{playersOnBench}</div>;
 };
 
 export default Bench;
