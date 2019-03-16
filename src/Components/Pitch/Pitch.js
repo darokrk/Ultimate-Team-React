8dc55e07b556ae98ec96980653acf47ae5b8d021
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-16 20:57:32 +0100

    Styled and displayed players on the field

--- src/Components/Pitch/Pitch.js
+++ src/Components/Pitch/Pitch.js
@@ -3,10 +3,16 @@ import Player from "../Player/Player";
 import "./Pitch.scss";
 
 const Pitch = props => {
-  const playersOnTheField = props.players.map(player => (
-    <Player key={player.name} player={player} />
+  const playersOnTheField = props.players.map((player, i) => (
+    <Player key={player.name} player={player} index={i} />
   ));
-  return <div className="pitch">{playersOnTheField}</div>;
+  return (
+    <div className="pitch">
+      <div className="field__wrapper field__wrapper-1-4-1-3-2">
+        {playersOnTheField}
+      </div>
+    </div>
+  );
 };
 
 export default Pitch;
--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Player.scss";
 
 const Player = props => {
-  return <div className="player">{props.player.name}</div>;
+  return (
+    <div className={`player__wrapper-${props.index}`}>
+      <div className="player" />
+      {props.player.name}
+    </div>
+  );
 };
 
 export default Player;
