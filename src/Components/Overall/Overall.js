1ae111515c01a32ee7b2dcd3c18c55055479f0b0
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-30 01:07:30 +0100

    Class names refactoring, added new elements

--- src/Components/Overall/Overall.js
+++ src/Components/Overall/Overall.js
@@ -3,8 +3,11 @@ import "./Overall.scss";
 
 const Overall = props => (
   <div className="overall__wrapper">
-    <div className="overall__slogan">Team Name</div>
-    <div className="overall__circle">Rating: {props.overallPower}</div>
+    <div className="overall__name">Team Name</div>
+    <div className="overall__power">Rating: {props.overallPower}</div>
+    <div className="overall__display">
+      <div className="display__content" />
+    </div>
   </div>
 );
 
