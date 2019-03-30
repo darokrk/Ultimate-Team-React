3ace0c72e4c12e67993df61d17742b39a81f130b
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-30 01:38:52 +0100

    Created formation select

--- src/Components/Overall/Overall.js
+++ src/Components/Overall/Overall.js
@@ -14,6 +14,14 @@ const Overall = props => (
         style={{ width: `${props.overallPower}%` }}
       />
     </div>
+    <select className="overall__formation">
+      <option value="1-4-1-3-2">1-4-1-3-2</option>
+      <option value="1-4-1-3-2">1-4-4-2</option>
+      <option value="1-4-1-3-2">1-4-1-2-1-2</option>
+      <option value="1-4-1-3-2">1-4-2-3-1</option>
+      <option value="1-4-1-3-2">1-4-2-2-2</option>
+      <option value="1-4-1-3-2">1-4-3-2-2</option>
+    </select>
   </div>
 );
 
