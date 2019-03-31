635c588a08aa9a4d4e57965b09c5baa7f4e2b8c4
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-31 22:20:14 +0200

    Added more skills attributes to players

--- src/Components/PlayerInfo/PlayerInfo.js
+++ src/Components/PlayerInfo/PlayerInfo.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Player from "../Player/Player";
+import "./PlayerInfo.scss";
 
 const PlayerInfo = props => {
-  return (
-    <div>
-      <div>Hello</div>
-    </div>
-  );
+  const { selectedPlayers } = props;
+
+  let playerInfo = null;
+
+  if (selectedPlayers[0]) {
+    playerInfo = (
+      <div className="player__more">
+        <div className="more__look">
+          <Player player={selectedPlayers[0]} />
+        </div>
+      </div>
+    );
+  }
+
+  return <>{playerInfo}</>;
 };
 
 export default PlayerInfo;
