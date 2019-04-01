5867b12553b5c48490e21261b00e92940d2fede9
Dariusz Rosiek <daro.krk@interia.pl>
2019-04-01 12:05:54 +0200

    Added stats object to every player

--- src/Components/PlayerInfo/PlayerInfo.js
+++ src/Components/PlayerInfo/PlayerInfo.js
@@ -8,11 +8,17 @@ const PlayerInfo = props => {
   let playerInfo = null;
 
   if (selectedPlayers[0]) {
+    let stats = selectedPlayers[0].stats;
+    stats = Object.keys(stats).map(element => (
+      <p>{`${element}: ${stats[element]}`}</p>
+    ));
+    console.log(stats);
     playerInfo = (
       <div className="player__more">
         <div className="more__look">
           <Player player={selectedPlayers[0]} />
         </div>
+        <div className="more__stats">{stats}</div>
       </div>
     );
   }
