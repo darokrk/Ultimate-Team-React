77a19dcc82e17db11438feedbf962a495a0be87f
Dariusz Rosiek <daro.krk@interia.pl>
2019-04-01 12:10:20 +0200

    Added key value to stats

--- src/Components/PlayerInfo/PlayerInfo.js
+++ src/Components/PlayerInfo/PlayerInfo.js
@@ -9,10 +9,9 @@ const PlayerInfo = props => {
 
   if (selectedPlayers[0]) {
     let stats = selectedPlayers[0].stats;
-    stats = Object.keys(stats).map(element => (
-      <p>{`${element}: ${stats[element]}`}</p>
+    stats = Object.keys(stats).map((statistic, i) => (
+      <div key={i}>{`${statistic}: ${stats[statistic]}`}</div>
     ));
-    console.log(stats);
     playerInfo = (
       <div className="player__more">
         <div className="more__look">
