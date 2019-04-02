f93b981262b7290f6084cb136860cb1f1c637e66
Dariusz Rosiek <daro.krk@interia.pl>
2019-04-02 14:40:45 +0200

    Created option to change manager on screen

--- src/Components/TeamPicker/TeamPicker.js
+++ src/Components/TeamPicker/TeamPicker.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
+import Manager from "../Manager/Manager";
+import managers from "../../data/managers.json";
+import "./TeamPicker.scss";
 
 class TeamPicker extends Component {
+  state = {
+    index: 0
+  };
+
+  handleClickNext = () => {
+    this.setState({
+      index: this.state.index + 1
+    });
+  };
+
+  handleClickPrevious = () => {
+    this.setState({
+      index: this.state.index - 1
+    });
+  };
+
+  componentDidUpdate() {
+    if (this.state.index > 5) {
+      this.setState({
+        index: 0
+      });
+    } else if (this.state.index < 0) {
+      this.setState({
+        index: 5
+      });
+    } else return;
+  }
+
   render() {
+    const managersList = managers.map(manager => (
+      <Manager key={manager.name} manager={manager} />
+    ));
     return (
       <div>
         <div>
           <h1>Welcome to ultimate team app</h1>
+          <span>Please choose your manager</span>
+        </div>
+        <div className="managers__wrapper">
+          {managersList[this.state.index]}
         </div>
         <div>
-          Please choose your manager
-          <ul>
-            <li />
-            <li />
-            <li />
-            <li />
-            <li />
-          </ul>
+          <button onClick={this.handleClickPrevious}>Previous</button>
+          <button onClick={this.handleClickNext}>Next</button>
         </div>
       </div>
     );
