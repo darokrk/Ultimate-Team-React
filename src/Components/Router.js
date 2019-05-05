d478f190e6159a73c0e0f2e5ac47860c84424939
Dariusz Rosiek <daro.krk@interia.pl>
2019-05-05 14:24:03 +0200

    Changed path in reactRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -6,8 +6,8 @@ import TeamPicker from "./TeamPicker/TeamPicker";
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={TeamPicker} />
-      <Route path="/team/:teamId" component={App} />
+      <Route exact path="/Ultimate-Team-React/" component={TeamPicker} />
+      <Route path="Ultimate-Team-React/team/:teamId" component={App} />
     </Switch>
   </BrowserRouter>
 );
